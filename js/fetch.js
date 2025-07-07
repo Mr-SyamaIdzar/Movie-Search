0c40c6d0e6642bc731d6e9f69fb2bb0b1c6aa510
Mr. Syamaidzar <muhammadraihansyamaidzar@gmail.com>
2025-07-07 21:09:33 +0700

    script: show movie to home

--- js/fetch.js
+++ js/fetch.js
@@ -1,12 +1,71 @@
-const API = `https://www.omdbapi.com/?apikey=df662044&t=hello+world `;
+async function fetchMovies(keyword) {
+  const apiKey = "df662044";
+  const hasilContainer = document.querySelector("#hasil");
+  hasilContainer.innerHTML = "";
 
-async function fetchMovies() {
   try {
-    const response = await axios.get(API);
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
+    // Fetch halaman pertama untuk tahu total halaman
+    const response = await axios.get(
+      `https://www.omdbapi.com/?apikey=${apiKey}&s=${keyword}&type=movie`
+    );
+    console.log(response);
+
+    // Tangani error Too many results
+    // if (
+    //   response.data.Response === "False" &&
+    //   response.data.Error === "Too many results."
+    // ) {
+    //   alert(
+    //     "Terlalu banyak hasil. Silakan gunakan kata kunci yang lebih spesifik."
+    //   );
+    //   return;
+    // }
+
+    if (response.data.Response === "True") {
+      // Loop setiap film dan ambil detail lengkap
+      const searchResults = response.data.Search;
+      for (let movie of searchResults) {
+        const detailRes = await axios.get(
+          `https://www.omdbapi.com/?apikey=${apiKey}&i=${movie.imdbID}`
+        );
+        const detail = detailRes.data;
+        console.log(detail);
+
+        // Buat elemen HTML berdasarkan detail
+        const cardHTML = `
+          <div class="card">
+            <img class="image" src="${
+              detail.Poster !== "N/A" ? detail.Poster : "placeholder.jpg"
+            }" alt="${detail.Title}" />
+            <div class="content">
+              <h2>${detail.Title}</h2>
+              <h3>${detail.Year}</h3>
+              <p>${detail.Plot}</p>
+              <a class="read-more" href="">Read more</a>
+              <div class="details">
+                <span class="item">
+                  <i class="fa-regular fa-star"></i>
+                  <em>${detail.imdbRating}/10</em>
+                </span>
+              </div>
+              <div class="buttons">
+                <a href="detail.html?id=${
+                  detail.imdbID
+                }" class="primary-btn">Search</a>
+              </div>
+            </div>
+          </div>
+        `;
+
+        hasilContainer.innerHTML += cardHTML;
+      }
+    } else {
+      hasilContainer.innerHTML = `<p>${response.data.Error}</p>`;
+    }
+  } catch (err) {
+    console.error(err);
+    hasilContainer.innerHTML = `<p>Terjadi kesalahan saat fetch data.</p>`;
   }
 }
 
-fetchMovies();
+fetchMovies("it");
